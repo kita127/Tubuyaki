208f2b3226e4d181f8107fd126ee05c7ed9883e4
kita127 <kitada.kosuke@gmail.com>
2024-01-08 22:50:20 +0900

    up: Vue側でログアウト実装

--- webapp/resources/js/components/App.vue
+++ webapp/resources/js/components/App.vue
@@ -2,7 +2,16 @@
     <div>
         <p>Vueによるメインページ</p>
     </div>
+    <div>
+        <button type="button" v-on:click="logout"> ログアウト </button>
+    </div>
 </template>
 
 <script lang="ts" setup>
+import axios from "axios";
+
+const logout = (): void => {
+    axios.post("logout");
+    window.location.href = "/login";
+};
 </script>
