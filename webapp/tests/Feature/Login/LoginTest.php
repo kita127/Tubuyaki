df8abf62a5be39865d3893ffe644aff128591b29
kita127 <kitada.kosuke@gmail.com>
2024-01-08 12:31:16 +0900

    up: いろいろ間違ってたので修正

--- webapp/tests/Feature/Login/LoginTest.php
+++ webapp/tests/Feature/Login/LoginTest.php
@@ -2,18 +2,48 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Hash;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class LoginTest extends TestCase
 {
+    use DatabaseTransactions;
+
+    /**
+     * LoginController::authenticate
+     */
     public function test01_01_メールアドレスとパスが一致したらリダイレクト(): void
     {
+        $user = new User([
+            'name' => 'test_user',
+            'email' => 'test_user@example.com',
+            'password' => Hash::make('testuserpass'),
+        ]);
+        $user->save();
+
         $response = $this->post('/login', [
-            'email' => 'admin@example.com',
-            'password',
+            'email' => 'test_user@example.com',
+            'password' => 'testuserpass',
         ]);
 
         $this->assertSame(302, $response->getStatusCode(), '認証成功後はリダイレクト');
+        //        $this->assertSame('', $response->headers);
     }
+
+    /**
+     * LoginController::authenticate
+     */
+    // public function test01_02_パスが不一致はxxx(): void
+    // {
+    //     $response = $this->post('/login', [
+    //         'email' => 'admin@example.com',
+    //         'invalidpass',
+    //     ]);
+
+    //     $this->assertSame(302, $response->getStatusCode(), '認証成功後はリダイレクト');
+    //     $this->assertSame([], $response->headers);
+    // }
 }
