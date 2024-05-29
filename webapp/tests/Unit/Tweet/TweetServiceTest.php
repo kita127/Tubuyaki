cf75c698a325e7fdac92da46a4685bbde995586e
kita127 <kitada.kosuke@gmail.com>
2024-05-29 16:08:13 +0900

    refactor

--- webapp/tests/Unit/Tweet/TweetServiceTest.php
+++ webapp/tests/Unit/Tweet/TweetServiceTest.php
@@ -3,12 +3,10 @@
 namespace Test\Unit\Tweet;
 
 use App\Repositories\User\MockUserRepository;
-use App\Repositories\User\UserRepository;
 use LogicException;
 use Tests\TestCase;
 use App\Entities\TweetType;
 use App\Repositories\Tweet\MockTweetRepository;
-use App\Repositories\Tweet\TweetRepository;
 use App\Services\Tweet\TweetRetriever;
 use App\Services\Tweet\TweetService;
 use Tests\Lib\TweetCreator;
@@ -16,51 +14,43 @@ use Tests\Lib\UserAssistance;
 
 class TweetServiceTest extends TestCase
 {
-    private readonly UserRepository $userRepository;
-    private readonly TweetRepository $tweetRepository;
-    private readonly UserAssistance $userAssistance;
     private readonly TweetRetriever $tweetRetriever;
     private readonly TweetCreator $tweetCreator;
+    private readonly UserAssistance $userAssistance;
+    private readonly TweetService $tweetService;
 
     protected function setUp(): void
     {
-        $this->tweetRepository = new MockTweetRepository();
-        $this->userRepository = new MockUserRepository();
-        $this->userAssistance = new UserAssistance($this->userRepository);
-        $this->tweetRetriever = new TweetRetriever($this->tweetRepository, $this->userRepository);
-        $this->tweetCreator = new TweetCreator($this->userRepository, $this->tweetRepository);
+        $tweetRepository = new MockTweetRepository();
+        $userRepository = new MockUserRepository();
+        $this->userAssistance = new UserAssistance($userRepository);
+        $this->tweetRetriever = new TweetRetriever($tweetRepository, $userRepository);
+        $this->tweetCreator = new TweetCreator($userRepository, $tweetRepository);
+        $this->tweetService = new TweetService($tweetRepository, $userRepository, $this->tweetRetriever);
     }
 
     public function test01_01_自分のつぶやきはリツイートできない(): void
     {
-        $service = $this->createService();
         $this->expectException(LogicException::class);
         $this->expectExceptionMessage('自分のつぶやきにはリツイートできません');
 
         $users = $this->userAssistance->createUsers(1);
         $me = $users->shift();
         $tweet = $this->tweetCreator->create($me, '自分のつぶやき', TweetType::Normal);
-        $service->retweet($tweet->id->value(), $me);
+        $this->tweetService->retweet($tweet->id->value(), $me);
     }
 
     // TODO: FeatureTestも書く
     public function test01_02_一度リツイートしたつぶやきに対してリツイートできない(): void
     {
-        $service = $this->createService();
-
         $users = $this->userAssistance->createUsers(2);
         $me = $users->shift();
         $other = $users->shift();
         $tweet = $this->tweetCreator->create($other, '他人のつぶやき', TweetType::Normal);
-        $service->retweet($tweet->id->value(), $me);
+        $this->tweetService->retweet($tweet->id->value(), $me);
 
         $this->expectException(LogicException::class);
         $this->expectExceptionMessage('同じつぶやきに再度リツイートしています');
-        $service->retweet($tweet->id->value(), $me);
-    }
-
-    private function createService(): TweetService
-    {
-        return new TweetService($this->tweetRepository, $this->userRepository, $this->tweetRetriever);
+        $this->tweetService->retweet($tweet->id->value(), $me);
     }
 }
