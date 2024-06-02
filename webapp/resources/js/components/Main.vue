890e98efa19c46926362ba17cfe4e6fb0cd83067
kita127 <kitada.kosuke@gmail.com>
2024-06-02 15:11:51 +0900

    Mainページ作成

--- webapp/resources/js/components/Main.vue
+++ webapp/resources/js/components/Main.vue
@@ -1,8 +1,34 @@
 <template>
     <div>Main Menu</div>
 </template>
+
 <script lang="ts" setup>
+import axios, { AxiosInstance } from "axios";
 import { ref } from "vue";
+import { onMounted } from 'vue'
+
+const me = async (http: AxiosInstance): Promise<void> => {
+    const a = await http.get("http://localhost/api/users/me");
+    console.log(a);
+};
+
+const timeline = () => {
+    //    axios.get();
+};
+
+onMounted(() => {
+    const http = axios.create({
+        baseURL: 'http://localhost',
+        withCredentials: true,
+    });
+
+    http.get('/api/users/me').then((res) => {
+        console.log(res);
+    });
+
+    console.log('ページが読み込まれました！')
+
+})
 
 </script>
 
